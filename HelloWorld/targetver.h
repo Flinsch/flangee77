763d5f1744b3e774c3707839af9b059671dfd413
Flinsch <flinsch@gmx.net>
2023-08-21 13:07:32 +0200

    Set minimum compatibility of demo application to Windows 7

--- HelloWorld/targetver.h
+++ HelloWorld/targetver.h
@@ -3,4 +3,6 @@
 // // Durch das Einschließen von "SDKDDKVer.h" wird die höchste verfügbare Windows-Plattform definiert.
 // Wenn Sie Ihre Anwendung für eine frühere Windows-Plattform erstellen möchten, schließen Sie "WinSDKVer.h" ein, und
 // legen Sie vor dem Einschließen von "SDKDDKVer.h" das _WIN32_WINNT-Makro auf die Plattform fest, die Sie unterstützen möchten.
+#include <WinSDKVer.h>
+#define _WIN32_WINNT _WIN32_WINNT_WIN7
 #include <SDKDDKVer.h>
