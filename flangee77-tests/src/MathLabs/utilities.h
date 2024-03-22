1a037696c988422fc01200501a41198ef3aee954
Flinsch <flinsch@gmx.net>
2024-03-22 09:45:20 +0100

    Add some power of two functions and corresponding tests

--- flangee77-tests/src/MathLabs/utilities.h
+++ flangee77-tests/src/MathLabs/utilities.h
@@ -193,4 +193,141 @@ TESTLABS_CASE( TEXT("MathLabs:  utilities:  round") )
 
 
 
+TESTLABS_CASE( TEXT("MathLabs:  utilities:  is_power_of_2") )
+{
+    for ( unsigned x = 0; x < 9999; ++x )
+    {
+        bool expected = false;
+        switch ( x )
+        {
+        case (1u <<  0):
+        case (1u <<  1):
+        case (1u <<  2):
+        case (1u <<  3):
+        case (1u <<  4):
+        case (1u <<  5):
+        case (1u <<  6):
+        case (1u <<  7):
+        case (1u <<  8):
+        case (1u <<  9):
+        case (1u << 10):
+        case (1u << 11):
+        case (1u << 12):
+        case (1u << 13):
+        case (1u << 14):
+        case (1u << 15):
+        case (1u << 16):
+        case (1u << 17):
+        case (1u << 18):
+        case (1u << 19):
+        case (1u << 20):
+        case (1u << 21):
+        case (1u << 22):
+        case (1u << 23):
+        case (1u << 24):
+        case (1u << 25):
+        case (1u << 26):
+        case (1u << 27):
+        case (1u << 28):
+        case (1u << 29):
+        case (1u << 30):
+        case (1u << 31):
+            expected = true;
+            break;
+        }
+
+        TESTLABS_CHECK_EQ( ml7::utilities::is_power_of_2( x ), expected );
+    }
+}
+
+TESTLABS_CASE( TEXT("MathLabs:  utilities:  prev_power_of_2") )
+{
+    struct Entry
+    {
+        unsigned x;
+        unsigned expected;
+    } entry;
+
+    const std::vector<Entry> container {
+        { 0, 0 },
+        { 1, 1 },
+        { 2, 2 },
+        { 3, 2 },
+        { 4, 4 },
+        { 5, 4 },
+        { 6, 4 },
+        { 7, 4 },
+        { 8, 8 },
+        { 9, 8 },
+        { 10, 8 },
+        { 11, 8 },
+        { 12, 8 },
+        { 13, 8 },
+        { 14, 8 },
+        { 15, 8 },
+        { 16, 16 },
+        { 17, 16 },
+        { 18, 16 },
+        { 19, 16 },
+        { 20, 16 },
+        { 0x7fffffff, 0x40000000 },
+        { 0x80000000, 0x80000000 },
+        { 0x80000001, 0x80000000 },
+        { 0xffffffff, 0x80000000 },
+    };
+
+    TESTLABS_SUBCASE_BATCH( TEXT("prev_power_of_2"), container, entry )
+    {
+        auto x = entry.x;
+        auto expected = entry.expected;
+        TESTLABS_CHECK_EQ( ml7::utilities::prev_power_of_2( x ), expected );
+    }
+}
+
+TESTLABS_CASE( TEXT("MathLabs:  utilities:  next_power_of_2") )
+{
+    struct Entry
+    {
+        unsigned x;
+        unsigned expected;
+    } entry;
+
+    const std::vector<Entry> container {
+        { 0, 1 },
+        { 1, 1 },
+        { 2, 2 },
+        { 3, 4 },
+        { 4, 4 },
+        { 5, 8 },
+        { 6, 8 },
+        { 7, 8 },
+        { 8, 8 },
+        { 9, 16 },
+        { 10, 16 },
+        { 11, 16 },
+        { 12, 16 },
+        { 13, 16 },
+        { 14, 16 },
+        { 15, 16 },
+        { 16, 16 },
+        { 17, 32 },
+        { 18, 32 },
+        { 19, 32 },
+        { 20, 32 },
+        { 0x7fffffff, 0x80000000 },
+        { 0x80000000, 0x80000000 },
+        { 0x80000001, 0x00000000 },
+        { 0xffffffff, 0x00000000 },
+    };
+
+    TESTLABS_SUBCASE_BATCH( TEXT("next_power_of_2"), container, entry )
+    {
+        auto x = entry.x;
+        auto expected = entry.expected;
+        TESTLABS_CHECK_EQ( ml7::utilities::next_power_of_2( x ), expected );
+    }
+}
+
+
+
 #endif // F77_TESTS_ML7_UTILITIES_H
--- flangee77/src/MathLabs/utilities.h
+++ flangee77/src/MathLabs/utilities.h
@@ -57,6 +57,7 @@ namespace utilities {
         return m;
     }
 
+
     /**
      * Clamps the given value to lie within the range [min; max].
      */
@@ -67,6 +68,7 @@ namespace utilities {
         if ( x >= max ) return max;
         return x;
     }
+
 
     /**
      * Rounds the given value to the closest "integer" value.
@@ -87,6 +89,52 @@ namespace utilities {
      * Rounds the given value to the specified number of decimal places.
      */
     double round(double x, unsigned num_decimals);
+
+
+    /**
+     * Checks whether the given value is a power of two.
+     */
+    template <typename T>
+        requires( std::is_integral_v<T> )
+    constexpr bool is_power_of_2(T x)
+    {
+        return x > 0 && (x & (x - 1)) == 0;
+    }
+
+    /**
+     * Returns the highest power of two value that is less than or equal to the
+     * specified non-zero value. If the value is 0, 0 is returned.
+     */
+    template <typename T>
+        requires( std::is_integral_v<T> )
+    constexpr T prev_power_of_2(T x)
+    {
+        constexpr T max = T(1) << (sizeof(T) * 8 - 1);
+        if ( x >= max )
+            return max;
+        unsigned y = 1;
+        while ( y <= x )
+            y <<= 1;
+        return y >> 1;
+    }
+
+    /**
+     * Returns the lowest power of two value that is greater than or equal to the
+     * specified value. If such a value does not exist (regarding integer overflow),
+     * 0 is returned.
+     */
+    template <typename T>
+        requires( std::is_integral_v<T> )
+    constexpr T next_power_of_2(T x)
+    {
+        constexpr T max = T(1) << (sizeof(T) * 8 - 1);
+        if ( x > max )
+            return 0;
+        unsigned y = 1;
+        while ( y < x )
+            y <<= 1;
+        return y;
+    }
 
 
 
