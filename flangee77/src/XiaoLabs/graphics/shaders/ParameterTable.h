bc053352d7f0d431e534aa3d29ccab7eae7f1010
Flinsch <flinsch@gmx.net>
2024-03-23 11:19:54 +0100

    Split parameter stuff across multiple files

--- flangee77/src/XiaoLabs/graphics/shaders/ParameterTable.h
+++ flangee77/src/XiaoLabs/graphics/shaders/ParameterTable.h
@@ -2,6 +2,8 @@
 #ifndef XL7_GRAPHICS_SHADERS_PARAMETERTABLE_H
 #define XL7_GRAPHICS_SHADERS_PARAMETERTABLE_H
 
+#include "./ParameterDesc.h"
+
 #include <CoreLabs/string.h>
 
 #include <map>
@@ -13,33 +15,6 @@ namespace graphics {
 namespace shaders {
 
 
-
-enum class ParameterType
-{
-    Invalid,
-    Bool,
-    Int,
-    Float,
-    Sampler,
-};
-
-struct ParameterDesc
-{
-    /** The parameter type. */
-    ParameterType type;
-
-    /** The register index. */
-    unsigned register_index;
-    /** The number of registers occupied. */
-    unsigned register_count;
-
-    /** The number of rows. */
-    unsigned row_count;
-    /** The number of columns. */
-    unsigned column_count;
-    /** The number of array elements. */
-    unsigned element_count;
-};
 
 typedef std::map<cl7::astring, ParameterDesc, std::less<>> ParameterTable;
 
