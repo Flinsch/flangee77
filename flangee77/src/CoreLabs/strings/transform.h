3d55085b6858deda7685d795028881485dd5dc54
Flinsch <flinsch@gmx.net>
2025-09-28 10:08:04 +0200

    Add some more (customizable) trimming functions (and respective tests); add comments

--- flangee77/src/CoreLabs/strings/transform.h
+++ flangee77/src/CoreLabs/strings/transform.h
@@ -11,6 +11,7 @@ namespace cl7::strings::transform {
 
 
 
+    /** Removes leading (Unicode) whitespace characters from the string in-place. */
     template <class Tstring>
         requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring>)
     void trim_left(Tstring& s)
@@ -20,6 +21,17 @@ namespace cl7::strings::transform {
         s.erase(0, k);
     }
 
+    /** Removes leading (Unicode) whitespace characters from the string in-place. */
+    template <class Tstring_view>
+        requires(is_any_string_view_v<Tstring_view> && !std::is_const_v<Tstring_view>)
+    void trim_left(Tstring_view& s)
+    {
+        const size_t k = inspect::count_whitespace_prefix(Tstring_view(s));
+        assert(k <= s.size());
+        s.remove_prefix(k);
+    }
+
+    /** Removes trailing (Unicode) whitespace characters from the string in-place. */
     template <class Tstring>
         requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring>)
     void trim_right(Tstring& s)
@@ -29,26 +41,19 @@ namespace cl7::strings::transform {
         s.erase(s.size() - k, k);
     }
 
+    /** Removes trailing (Unicode) whitespace characters from the string in-place. */
     template <class Tstring_view>
-        requires(is_any_string_view_v<Tstring_view>)
-    void trim_left(Tstring_view& s)
-    {
-        const size_t k = inspect::count_whitespace_prefix(Tstring_view(s));
-        assert(k <= s.size());
-        s = s.substr(k);
-    }
-
-    template <class Tstring_view>
-        requires(is_any_string_view_v<Tstring_view>)
+        requires(is_any_string_view_v<Tstring_view> && !std::is_const_v<Tstring_view>)
     void trim_right(Tstring_view& s)
     {
         const size_t k = inspect::count_whitespace_suffix(Tstring_view(s));
         assert(k <= s.size());
-        s = s.substr(0, s.size() - k);
+        s.remove_suffix(k);
     }
 
+    /** Removes both leading and trailing (Unicode) whitespace characters from the string in-place. */
     template <class Tstring_or_view>
-        requires(is_any_string_or_view_v<Tstring_or_view>)
+        requires(is_any_string_or_view_v<Tstring_or_view> && !std::is_const_v<Tstring_or_view>)
     void trim(Tstring_or_view& s)
     {
         trim_right(s);
@@ -57,6 +62,101 @@ namespace cl7::strings::transform {
 
 
 
+    /** Removes leading occurrences of the specified character from the string in-place. */
+    template <class Tstring, typename Tchar = Tstring::value_type>
+        requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring> && std::is_integral_v<Tchar>)
+    void trim_left(Tstring& s, Tchar c)
+    {
+        while (!s.empty() && s.front() == c)
+            s.erase(0, 1);
+    }
+
+    /** Removes leading occurrences of the specified character from the string in-place. */
+    template <class Tstring_view, typename Tchar = Tstring_view::value_type>
+        requires(is_any_string_view_v<Tstring_view> && !std::is_const_v<Tstring_view> && std::is_integral_v<Tchar>)
+    void trim_left(Tstring_view& s, Tchar c)
+    {
+        while (!s.empty() && s.front() == c)
+            s.remove_prefix(1);
+    }
+
+    /** Removes trailing occurrences of the specified character from the string in-place. */
+    template <class Tstring, typename Tchar = Tstring::value_type>
+        requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring> && std::is_integral_v<Tchar>)
+    void trim_right(Tstring& s, Tchar c)
+    {
+        while (!s.empty() && s.back() == c)
+            s.erase(s.size() - 1, 1);
+    }
+
+    /** Removes trailing occurrences of the specified character from the string in-place. */
+    template <class Tstring_view, typename Tchar = Tstring_view::value_type>
+        requires(is_any_string_view_v<Tstring_view> && !std::is_const_v<Tstring_view> && std::is_integral_v<Tchar>)
+    void trim_right(Tstring_view& s, Tchar c)
+    {
+        while (!s.empty() && s.back() == c)
+            s.remove_suffix(1);
+    }
+
+    /** Removes both leading and trailing occurrences of the specified character from the string in-place. */
+    template <class Tstring_or_view, typename Tchar = Tstring_or_view::value_type>
+        requires(is_any_string_or_view_v<Tstring_or_view> && !std::is_const_v<Tstring_or_view> && std::is_integral_v<Tchar>)
+    void trim(Tstring_or_view& s, Tchar c)
+    {
+        trim_right(s, c);
+        trim_left(s, c);
+    }
+
+
+
+    /** Removes leading occurrences of the specified characters from the string in-place. */
+    template <class Tstring, class Tstring_view>
+        requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring> && is_any_string_view_v<Tstring_view> && std::same_as<typename Tstring::value_type, typename Tstring_view::value_type>)
+    void trim_left(Tstring& s, Tstring_view chars)
+    {
+        while (!s.empty() && chars.find(s.front()) != Tstring_view::npos)
+            s.erase(0, 1);
+    }
+
+    /** Removes leading occurrences of the specified characters from the string in-place. */
+    template <class Tstring_view>
+        requires(is_any_string_view_v<Tstring_view> && !std::is_const_v<Tstring_view>)
+    void trim_left(Tstring_view& s, Tstring_view chars)
+    {
+        while (!s.empty() && chars.find(s.front()) != Tstring_view::npos)
+            s.remove_prefix(1);
+    }
+
+    /** Removes trailing occurrences of the specified characters from the string in-place. */
+    template <class Tstring, class Tstring_view>
+        requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring> && is_any_string_view_v<Tstring_view> && std::same_as<typename Tstring::value_type, typename Tstring_view::value_type>)
+    void trim_right(Tstring& s, Tstring_view chars)
+    {
+        while (!s.empty() && chars.find(s.back()) != Tstring_view::npos)
+            s.erase(s.size() - 1, 1);
+    }
+
+    /** Removes trailing occurrences of the specified characters from the string in-place. */
+    template <class Tstring_view>
+        requires(is_any_string_view_v<Tstring_view> && !std::is_const_v<Tstring_view>)
+    void trim_right(Tstring_view& s, Tstring_view chars)
+    {
+        while (!s.empty() && chars.find(s.back()) != Tstring_view::npos)
+            s.remove_suffix(1);
+    }
+
+    /** Removes both leading and trailing occurrences of the specified characters from the string in-place. */
+    template <class Tstring_or_view, class Tstring_view>
+        requires(is_any_string_or_view_v<Tstring_or_view> && !std::is_const_v<Tstring_or_view> && is_any_string_view_v<Tstring_view> && std::same_as<typename Tstring_or_view::value_type, typename Tstring_view::value_type>)
+    void trim(Tstring_or_view& s, Tstring_view chars)
+    {
+        trim_right(s, chars);
+        trim_left(s, chars);
+    }
+
+
+
+    /** Returns (a copy of) the string with leading (Unicode) whitespace removed. */
     template <class Tstring_or_view>
         requires(is_any_string_or_view_v<Tstring_or_view>)
     Tstring_or_view trimmed_left(Tstring_or_view s)
@@ -65,6 +165,7 @@ namespace cl7::strings::transform {
         return s;
     }
 
+    /** Returns (a copy of) the string with trailing (Unicode) whitespace removed. */
     template <class Tstring_or_view>
         requires(is_any_string_or_view_v<Tstring_or_view>)
     Tstring_or_view trimmed_right(Tstring_or_view s)
@@ -73,6 +174,7 @@ namespace cl7::strings::transform {
         return s;
     }
 
+    /** Returns (a copy of) the string with leading and trailing (Unicode) whitespace removed. */
     template <class Tstring_or_view>
         requires(is_any_string_or_view_v<Tstring_or_view>)
     Tstring_or_view trimmed(Tstring_or_view s)
@@ -83,6 +185,64 @@ namespace cl7::strings::transform {
 
 
 
+    /** Returns (a copy of) the string with leading occurrences of the specified character removed. */
+    template <class Tstring_or_view, typename Tchar = Tstring_or_view::value_type>
+        requires(is_any_string_or_view_v<Tstring_or_view> && std::is_integral_v<Tchar>)
+    Tstring_or_view trimmed_left(Tstring_or_view s, Tchar c)
+    {
+        trim_left(s, c);
+        return s;
+    }
+
+    /** Returns (a copy of) the string with trailing occurrences of the specified character removed. */
+    template <class Tstring_or_view, typename Tchar = Tstring_or_view::value_type>
+        requires(is_any_string_or_view_v<Tstring_or_view> && std::is_integral_v<Tchar>)
+    Tstring_or_view trimmed_right(Tstring_or_view s, Tchar c)
+    {
+        trim_right(s, c);
+        return s;
+    }
+
+    /** Returns (a copy of) the string with leading and trailing occurrences of the specified character removed. */
+    template <class Tstring_or_view, typename Tchar = Tstring_or_view::value_type>
+        requires(is_any_string_or_view_v<Tstring_or_view> && std::is_integral_v<Tchar>)
+    Tstring_or_view trimmed(Tstring_or_view s, Tchar c)
+    {
+        trim(s, c);
+        return s;
+    }
+
+
+
+    /** Returns (a copy of) the string with leading occurrences of the specified characters removed. */
+    template <class Tstring_or_view, class Tstring_view>
+        requires(is_any_string_or_view_v<Tstring_or_view> && is_any_string_view_v<Tstring_view> && std::same_as<typename Tstring_or_view::value_type, typename Tstring_view::value_type>)
+    Tstring_or_view trimmed_left(Tstring_or_view s, Tstring_view chars)
+    {
+        trim_left(s, chars);
+        return s;
+    }
+
+    /** Returns (a copy of) the string with trailing occurrences of the specified characters removed. */
+    template <class Tstring_or_view, class Tstring_view>
+        requires(is_any_string_or_view_v<Tstring_or_view> && is_any_string_view_v<Tstring_view> && std::same_as<typename Tstring_or_view::value_type, typename Tstring_view::value_type>)
+    Tstring_or_view trimmed_right(Tstring_or_view s, Tstring_view chars)
+    {
+        trim_right(s, chars);
+        return s;
+    }
+
+    /** Returns (a copy of) the string with leading and trailing occurrences of the specified characters removed. */
+    template <class Tstring_or_view, class Tstring_view>
+        requires(is_any_string_or_view_v<Tstring_or_view> && is_any_string_view_v<Tstring_view> && std::same_as<typename Tstring_or_view::value_type, typename Tstring_view::value_type>)
+    Tstring_or_view trimmed(Tstring_or_view s, Tstring_view chars)
+    {
+        trim(s, chars);
+        return s;
+    }
+
+
+    /** Pads the string on the left with the specified character until it has at least the desired length. */
     template <class Tstring, typename Tchar = Tstring::value_type>
         requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring> && std::is_integral_v<Tchar>)
     void pad_left(Tstring& s, size_t min_length, Tchar c)
@@ -91,6 +251,7 @@ namespace cl7::strings::transform {
             s.insert(s.begin(), min_length - s.length(), c);
     }
 
+    /** Pads the string on the right with the specified character until it has at least the desired length. */
     template <class Tstring, typename Tchar = Tstring::value_type>
         requires(is_any_string_v<Tstring> && !std::is_const_v<Tstring> && std::is_integral_v<Tchar>)
     void pad_right(Tstring& s, size_t min_length, Tchar c)
@@ -101,6 +262,7 @@ namespace cl7::strings::transform {
 
 
 
+    /** Returns a copy of the string left-padded with the specified character to reach the desired minimum length. */
     template <class Tstring, typename Tchar = Tstring::value_type>
         requires(is_any_string_v<Tstring> && std::is_integral_v<Tchar>)
     Tstring padded_left(Tstring s, size_t min_length, Tchar c)
@@ -109,6 +271,7 @@ namespace cl7::strings::transform {
         return s;
     }
 
+    /** Returns a copy of the string right-padded with the specified character to reach the desired minimum length. */
     template <class Tstring, typename Tchar = Tstring::value_type>
         requires(is_any_string_v<Tstring> && std::is_integral_v<Tchar>)
     Tstring padded_right(Tstring s, size_t min_length, Tchar c)
@@ -119,6 +282,7 @@ namespace cl7::strings::transform {
 
 
 
+    /** Converts a single ASCII character to lowercase. */
     template <typename Tchar>
         requires(std::is_integral_v<Tchar>)
     Tchar to_lower_ascii(Tchar c)
@@ -128,6 +292,7 @@ namespace cl7::strings::transform {
         return c;
     }
 
+    /** Converts a single ASCII character to uppercase. */
     template <typename Tchar>
         requires(std::is_integral_v<Tchar>)
     Tchar to_upper_ascii(Tchar c)
@@ -139,6 +304,7 @@ namespace cl7::strings::transform {
 
 
 
+    /** Converts all ASCII letters in the string to lowercase in-place. */
     template <class Tstring_or_span>
         requires(is_any_string_or_span_v<Tstring_or_span> && !std::is_const_v<Tstring_or_span>)
     Tstring_or_span& to_lower_ascii_inplace(Tstring_or_span& s)
@@ -151,6 +317,7 @@ namespace cl7::strings::transform {
         return s;
     }
 
+    /** Converts all ASCII letters in the string to uppercase in-place. */
     template <class Tstring_or_span>
         requires(is_any_string_or_span_v<Tstring_or_span> && !std::is_const_v<Tstring_or_span>)
     Tstring_or_span& to_upper_ascii_inplace(Tstring_or_span& s)
@@ -165,6 +332,7 @@ namespace cl7::strings::transform {
 
 
 
+    /** Returns a copy of the string with all ASCII letters converted to lowercase. */
     template <class Tstring_or_view, class Tstring = std::basic_string<typename Tstring_or_view::value_type>>
         requires(is_any_string_or_view_v<Tstring_or_view>)
     Tstring to_lower_ascii(Tstring_or_view s)
@@ -174,6 +342,7 @@ namespace cl7::strings::transform {
         return s_;
     }
 
+    /** Returns a copy of the string with all ASCII letters converted to uppercase. */
     template <class Tstring_or_view, class Tstring = std::basic_string<typename Tstring_or_view::value_type>>
         requires(is_any_string_or_view_v<Tstring_or_view>)
     Tstring to_upper_ascii(Tstring_or_view s)
