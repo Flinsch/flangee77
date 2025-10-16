f4f61c12845ae8ae3b175ed1a91a7200f2e65363
Flinsch <flinsch@gmx.net>
2025-10-16 18:22:50 +0200

    Introduce `make_byte_view_contiguous`

--- flangee77/src/CoreLabs/byte_view.h
+++ flangee77/src/CoreLabs/byte_view.h
@@ -28,17 +28,55 @@ namespace cl7 {
     }
 
     template <typename T>
-        requires(std::is_same_v<std::remove_cvref_t<T>, T> && std::is_trivially_copyable_v<T>)
-    auto make_byte_view(const T* obj)
-    {
-        return byte_view(reinterpret_cast<const std::byte*>(obj), sizeof(T));
-    }
-
-    template <typename T>
         requires(std::is_same_v<std::remove_cvref_t<T>, T> && sizeof(T) == sizeof(std::byte))
     auto make_byte_view(const T* data, size_t size)
     {
         return byte_view(reinterpret_cast<const std::byte*>(data), size);
+    }
+
+    template <typename T>
+        requires(std::is_same_v<std::remove_cvref_t<T>, T> && std::is_trivially_copyable_v<T> && sizeof(T) == sizeof(std::byte))
+    auto make_byte_view(const T* obj)
+    {
+        return byte_view(reinterpret_cast<const std::byte*>(obj), 1);
+    }
+
+
+
+    template <class T>
+        requires(requires {
+            typename T::value_type;
+        } && requires (const T& v) {
+            { v.data() } -> std::same_as<const typename T::value_type*>;
+            { v.size() } -> std::convertible_to<size_t>;
+        })
+    auto make_byte_view_contiguous(const T& v)
+    {
+        if (v.size() > 1) {
+            const auto* b0 = reinterpret_cast<const std::byte*>(&v[0]);
+            const auto* b1 = reinterpret_cast<const std::byte*>(&v[1]);
+            assert(b1 == b0 + sizeof(T));
+        }
+        return byte_view(reinterpret_cast<const std::byte*>(v.data()), v.size() * sizeof(typename T::value_type));
+    }
+
+    template <typename T>
+        requires(std::is_same_v<std::remove_cvref_t<T>, T>)
+    auto make_byte_view_contiguous(const T* array, size_t count)
+    {
+        if (count > 1) {
+            const auto* b0 = reinterpret_cast<const std::byte*>(&array[0]);
+            const auto* b1 = reinterpret_cast<const std::byte*>(&array[1]);
+            assert(b1 == b0 + sizeof(T));
+        }
+        return byte_view(reinterpret_cast<const std::byte*>(array), count * sizeof(T));
+    }
+
+    template <typename T>
+        requires(std::is_same_v<std::remove_cvref_t<T>, T> && std::is_trivially_copyable_v<T>)
+    auto make_byte_view_contiguous(const T* obj)
+    {
+        return byte_view(reinterpret_cast<const std::byte*>(obj), sizeof(T));
     }
 
 
