55172ca694992da1b3d81f4b7a0a64f1134699bd
Flinsch <flinsch@gmx.net>
2025-10-18 10:39:50 +0200

    Add attribute name, skip most constructors for now, add "missing" constructor/function implementations

--- flangee77/src/DataLabs/xml/Attribute.cpp
+++ flangee77/src/DataLabs/xml/Attribute.cpp
@@ -6,6 +6,18 @@ namespace dl7::xml {
 
 
 
+    Attribute::Attribute(cl7::u8string name, cl7::u8string value)
+        : _name(std::move(name))
+        , _value(std::move(value))
+    {
+    }
+
+    void Attribute::swap(Attribute& other) noexcept
+    {
+        _name.swap(other._name);
+        _value.swap(other._value);
+    }
+
 
 
 } // namespace dl7::xml
--- flangee77/src/DataLabs/xml/Attribute.h
+++ flangee77/src/DataLabs/xml/Attribute.h
@@ -14,21 +14,7 @@ class Attribute
 
 public:
     Attribute() noexcept = default;
-    explicit Attribute(cl7::u8string string);
-    explicit Attribute(cl7::u8string_view string);
-    explicit Attribute(const cl7::u8char_t* string);
-
-    explicit Attribute(float number);
-    explicit Attribute(double number);
-    explicit Attribute(long double number);
-
-    explicit Attribute(signed number);
-    explicit Attribute(signed long number);
-    explicit Attribute(signed long long number);
-
-    explicit Attribute(unsigned number);
-    explicit Attribute(unsigned long number);
-    explicit Attribute(unsigned long long number);
+    explicit Attribute(cl7::u8string name, cl7::u8string string);
 
     void swap(Attribute& other) noexcept;
 
@@ -38,18 +24,30 @@ public:
      * Returns true if the attribute value represents an empty string; returns false
      * otherwise (i.e., strings like "0" etc. are also considered non-empty).
      */
-    bool is_empty() const noexcept;
+    bool is_empty() const noexcept { return _value.empty(); }
 
 
 
+    /** Returns the name of this attribute. */
+    const cl7::u8string& get_name() const { return _name; }
+
+    /** Sets the name of this attribute. */
+    template <cl7::string_constructible<cl7::u8string> Tstring>
+    void set_name(Tstring&& name) { _name = cl7::u8string(std::forward<Tstring>(name)); }
+
+
+
+    /** Returns the string value of this attribute. */
     const cl7::u8string& get_value() const { return _value; }
 
+    /** Sets the string value of this attribute. */
     template <cl7::string_constructible<cl7::u8string> Tstring>
     void set_value(Tstring&& value) { _value = cl7::u8string(std::forward<Tstring>(value)); }
 
 
 
 private:
+    cl7::u8string _name;
     cl7::u8string _value;
 
 }; // class Attribute
