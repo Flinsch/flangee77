f2deab4a8e297b1be9ac5be0a9302062253c6a13
Flinsch <flinsch@gmx.net>
2025-10-21 20:14:53 +0200

    Remove those spaces

--- flangee77/src/DataLabs/ini/detail/Lexer.cpp
+++ flangee77/src/DataLabs/ini/detail/Lexer.cpp
@@ -21,32 +21,32 @@ namespace dl7::ini::detail {
 
     void Lexer::_init_symbols()
     {
-        _terminal_symbols.add_literal( WHITESPACE, u8" " );
-        _terminal_symbols.add_literal( WHITESPACE, u8"\t" );
-        _terminal_symbols.add_literal( NEWLINE, u8"\r\n" );
-        _terminal_symbols.add_literal( NEWLINE, u8"\n" );
-        _terminal_symbols.add_literal( NEWLINE, u8"\r" );
-        _terminal_symbols.add_literal( COMMENT_DELIMITER, u8";" );
-        _terminal_symbols.add_literal( COMMENT_DELIMITER, u8"#" );
-        _terminal_symbols.add_literal( SECTION_NAME_DELIMITER, u8"." );
-        _terminal_symbols.add_literal( SECTION_NAME_DELIMITER, u8"/" );
-        _terminal_symbols.add_literal( KEY_VALUE_DELIMITER, u8"=" );
-        _terminal_symbols.add_literal( KEY_VALUE_DELIMITER, u8":" );
-        _terminal_symbols.add_literal( OPENING_BRACKET, u8"[" );
-        _terminal_symbols.add_literal( CLOSING_BRACKET, u8"]" );
-        _terminal_symbols.add_literal( ESCAPED_CHARACTER, u8"\\t" );
-        _terminal_symbols.add_literal( ESCAPED_CHARACTER, u8"\\n" );
-        _terminal_symbols.add_literal( ESCAPED_CHARACTER, u8"\\r" );
-        _terminal_symbols.add_literal( ESCAPED_CHARACTER, u8"\\\"" );
-        _terminal_symbols.add_literal( ESCAPED_CHARACTER, u8"\\\\" );
-        _terminal_symbols.add_literal( ESCAPED_NEWLINE, u8"\\\r\n" );
-        _terminal_symbols.add_literal( ESCAPED_NEWLINE, u8"\\\n" );
-        _terminal_symbols.add_literal( ESCAPED_NEWLINE, u8"\\\r" );
+        _terminal_symbols.add_literal(WHITESPACE, u8" ");
+        _terminal_symbols.add_literal(WHITESPACE, u8"\t");
+        _terminal_symbols.add_literal(NEWLINE, u8"\r\n");
+        _terminal_symbols.add_literal(NEWLINE, u8"\n");
+        _terminal_symbols.add_literal(NEWLINE, u8"\r");
+        _terminal_symbols.add_literal(COMMENT_DELIMITER, u8";");
+        _terminal_symbols.add_literal(COMMENT_DELIMITER, u8"#");
+        _terminal_symbols.add_literal(SECTION_NAME_DELIMITER, u8".");
+        _terminal_symbols.add_literal(SECTION_NAME_DELIMITER, u8"/");
+        _terminal_symbols.add_literal(KEY_VALUE_DELIMITER, u8"=");
+        _terminal_symbols.add_literal(KEY_VALUE_DELIMITER, u8":");
+        _terminal_symbols.add_literal(OPENING_BRACKET, u8"[");
+        _terminal_symbols.add_literal(CLOSING_BRACKET, u8"]");
+        _terminal_symbols.add_literal(ESCAPED_CHARACTER, u8"\\t");
+        _terminal_symbols.add_literal(ESCAPED_CHARACTER, u8"\\n");
+        _terminal_symbols.add_literal(ESCAPED_CHARACTER, u8"\\r");
+        _terminal_symbols.add_literal(ESCAPED_CHARACTER, u8"\\\"");
+        _terminal_symbols.add_literal(ESCAPED_CHARACTER, u8"\\\\");
+        _terminal_symbols.add_literal(ESCAPED_NEWLINE, u8"\\\r\n");
+        _terminal_symbols.add_literal(ESCAPED_NEWLINE, u8"\\\n");
+        _terminal_symbols.add_literal(ESCAPED_NEWLINE, u8"\\\r");
 
-        _terminal_symbols.add_custom( QUOTED_STRING_LITERAL, syntax::matchers::DoubleQuotedStringMatcher{} );
-        _terminal_symbols.add_custom( QUOTED_STRING_LITERAL, syntax::matchers::SingleQuotedStringMatcher{} );
+        _terminal_symbols.add_custom(QUOTED_STRING_LITERAL, syntax::matchers::DoubleQuotedStringMatcher{});
+        _terminal_symbols.add_custom(QUOTED_STRING_LITERAL, syntax::matchers::SingleQuotedStringMatcher{});
 
-        _terminal_symbols.add_custom( ANY_OTHER, AnyOtherMatcher{} );
+        _terminal_symbols.add_custom(ANY_OTHER, AnyOtherMatcher{});
     }
 
 
--- flangee77/src/DataLabs/json/detail/Lexer.cpp
+++ flangee77/src/DataLabs/json/detail/Lexer.cpp
@@ -21,27 +21,27 @@ namespace dl7::json::detail {
 
     void Lexer::_init_symbols()
     {
-        _terminal_symbols.add_literal( NULL_CONSTANT, u8"null" );
-        _terminal_symbols.add_literal( TRUE_CONSTANT, u8"true" );
-        _terminal_symbols.add_literal( FALSE_CONSTANT, u8"false" );
+        _terminal_symbols.add_literal(NULL_CONSTANT, u8"null");
+        _terminal_symbols.add_literal(TRUE_CONSTANT, u8"true");
+        _terminal_symbols.add_literal(FALSE_CONSTANT, u8"false");
 
-        _terminal_symbols.add_custom( INTEGER_LITERAL, syntax::matchers::IntegerLiteralMatcher{} );
-        _terminal_symbols.add_custom( FLOAT_LITERAL, syntax::matchers::FloatingPointLiteralMatcher{} );
-        _terminal_symbols.add_custom( STRING_LITERAL, syntax::matchers::DoubleQuotedStringMatcher{} );
-        _terminal_symbols.add_custom( STRING_LITERAL, syntax::matchers::SingleQuotedStringMatcher{} );
+        _terminal_symbols.add_custom(INTEGER_LITERAL, syntax::matchers::IntegerLiteralMatcher{});
+        _terminal_symbols.add_custom(FLOAT_LITERAL, syntax::matchers::FloatingPointLiteralMatcher{});
+        _terminal_symbols.add_custom(STRING_LITERAL, syntax::matchers::DoubleQuotedStringMatcher{});
+        _terminal_symbols.add_custom(STRING_LITERAL, syntax::matchers::SingleQuotedStringMatcher{});
 
-        _terminal_symbols.add_custom( IDENTIFIER, IdentifierMatcher{} );
+        _terminal_symbols.add_custom(IDENTIFIER, IdentifierMatcher{});
 
-        _terminal_symbols.add_literal( LEFT_CURLY_BRACKET, u8"{" );
-        _terminal_symbols.add_literal( RIGHT_CURLY_BRACKET, u8"}" );
-        _terminal_symbols.add_literal( LEFT_SQUARE_BRACKET, u8"[" );
-        _terminal_symbols.add_literal( RIGHT_SQUARE_BRACKET, u8"]" );
+        _terminal_symbols.add_literal(LEFT_CURLY_BRACKET, u8"{");
+        _terminal_symbols.add_literal(RIGHT_CURLY_BRACKET, u8"}");
+        _terminal_symbols.add_literal(LEFT_SQUARE_BRACKET, u8"[");
+        _terminal_symbols.add_literal(RIGHT_SQUARE_BRACKET, u8"]");
 
-        _terminal_symbols.add_literal( COMMA, u8"," );
-        _terminal_symbols.add_literal( COLON, u8":" );
+        _terminal_symbols.add_literal(COMMA, u8",");
+        _terminal_symbols.add_literal(COLON, u8":");
 
-        _terminal_symbols.add_custom( LINE_COMMENT, syntax::matchers::LineCommentMatcher{} );
-        _terminal_symbols.add_custom( BLOCK_COMMENT, syntax::matchers::BlockCommentMatcher{} );
+        _terminal_symbols.add_custom(LINE_COMMENT, syntax::matchers::LineCommentMatcher{});
+        _terminal_symbols.add_custom(BLOCK_COMMENT, syntax::matchers::BlockCommentMatcher{});
     }
 
 
