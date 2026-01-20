f51057422915862656030468a46251982f054c91
Flinsch <Flinsch@users.noreply.github.com>
2026-01-20 20:16:22 +0000

    Add `write_char`

--- d6/dba/classcl7_1_1io_1_1Utf8Writer.js
+++ d6/dba/classcl7_1_1io_1_1Utf8Writer.js
@@ -2,6 +2,7 @@ var classcl7_1_1io_1_1Utf8Writer =
 [
     [ "Utf8Writer", "d6/dba/classcl7_1_1io_1_1Utf8Writer.html#a351c5fd087071bc450c92b7de2732251", null ],
     [ "write", "d6/dba/classcl7_1_1io_1_1Utf8Writer.html#a8fc26177a4b6cd9600f54e35103cbace", null ],
+    [ "write_char", "d6/dba/classcl7_1_1io_1_1Utf8Writer.html#a9291f7d65e559efa792dd657a090b3f7", null ],
     [ "write_line", "d6/dba/classcl7_1_1io_1_1Utf8Writer.html#a1a0b8f6d54efe905b3e5d658a089dc0a", null ],
     [ "_writable", "d6/dba/classcl7_1_1io_1_1Utf8Writer.html#a436e3294712823b3c4e2c4cde42cf07f", null ]
 ];
