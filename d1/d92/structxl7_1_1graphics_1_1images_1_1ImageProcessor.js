f24a58da4742b5dc5eae52b9b2979906cea46586
Flinsch <Flinsch@users.noreply.github.com>
2026-03-29 07:32:57 +0000

    Introduce `ImageProcessor::flip_vertically`

--- d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.js
+++ d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.js
@@ -1,7 +1,8 @@
 var structxl7_1_1graphics_1_1images_1_1ImageProcessor =
 [
-    [ "_pack_color", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#abb5b0bf2550af7eb061a397b9687632e", null ],
-    [ "_unpack_color", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#ad7cae95b8bdcfd059abd848b1dbc5ebe", null ],
+    [ "flip_vertically", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#ad39664cbf1534e4724f588d200ff1460", null ],
+    [ "pack_color", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#a2bb0d288b9f98c085b0258094b427715", null ],
     [ "pack_color", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#a2f51289e6d523e8a408990bf9e2e23a9", null ],
+    [ "unpack_color", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#a5c85f55efbb0cf214504a383a67fe2ee", null ],
     [ "unpack_color", "d1/d92/structxl7_1_1graphics_1_1images_1_1ImageProcessor.html#afec00347c49b5c36ebffa29903aeec9c", null ]
 ];
