4c4dc887a959489bbafe6b9b95a0c10b65d1e496
Flinsch <Flinsch@users.noreply.github.com>
2026-07-01 20:12:05 +0000

    Add overloaded function for fixed-size arrays; switch from object pointer to object reference

--- d7/d56/byte__span_8h.js
+++ d7/d56/byte__span_8h.js
@@ -1,7 +1,8 @@
 var byte__span_8h =
 [
     [ "byte_span", "d7/d56/byte__span_8h.html#acdad82ceadf0df7bee113b825df98a5d", null ],
+    [ "make_byte_span", "d7/d56/byte__span_8h.html#a9986e83b86a86e74a523cf1de6456957", null ],
     [ "make_byte_span", "d7/d56/byte__span_8h.html#ac3835244102d1cac6e7887eabe9fdd83", null ],
     [ "make_byte_span", "d7/d56/byte__span_8h.html#a3d2cea74548f9358781dcaa8cf8369f9", null ],
-    [ "make_byte_span", "d7/d56/byte__span_8h.html#a07af663869f065a2bcfeaa75179cbb49", null ]
+    [ "make_byte_span", "d7/d56/byte__span_8h.html#a94728c9d284f5a16b3b0f71ed50a5721", null ]
 ];
--- d9/d58/byte__view_8h.js
+++ d9/d58/byte__view_8h.js
@@ -3,5 +3,6 @@ var byte__view_8h =
     [ "byte_view", "d9/d58/byte__view_8h.html#a392a212050307677351feba78ccf36e6", null ],
     [ "make_byte_view", "d9/d58/byte__view_8h.html#a0b7e06efb4050b9bdea3f8c723deff7b", null ],
     [ "make_byte_view", "d9/d58/byte__view_8h.html#a2234edd7343d710bc11339d043e91d0c", null ],
-    [ "make_byte_view", "d9/d58/byte__view_8h.html#a4aff1bd9a8c0f558541a3b15c6a075db", null ]
+    [ "make_byte_view", "d9/d58/byte__view_8h.html#a38f2d0e53eebaec647c9e907df56782d", null ],
+    [ "make_byte_view", "d9/d58/byte__view_8h.html#ae640c4766ccb5b50f1ac86863496e7de", null ]
 ];
